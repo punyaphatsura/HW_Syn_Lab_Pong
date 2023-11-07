07ac17bba2d08398e6967193ce6bdb0e80f16852
punyaphatsura <punyaphat.sura@gmail.com>
2023-11-07 12:57:18 +0700

    Delete unused code

--- Pong.srcs/sources_1/new/baudrate_gen.v
+++ Pong.srcs/sources_1/new/baudrate_gen.v
@@ -29,11 +29,6 @@ module baudrate_gen(
     always @(posedge clk) begin
         counter = counter + 1;
         if (counter == 325) begin counter = 0; baud = ~baud; end 
-        // Clock = 10ns
-        // ClockFreq = 1/10ns = 100 MHz
-        // Baudrate = 9600
-        // counter = ClockFreq/Baudrate/16/2
-        // sampling every 16 ticks
     end
     
 endmodule
