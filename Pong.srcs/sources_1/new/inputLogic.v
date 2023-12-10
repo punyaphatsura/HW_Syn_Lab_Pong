b91a3a4dd7f39c6e2495075ec99e091a962503cb
punyaphatsura <punyaphat.sura@gmail.com>
2023-12-10 20:42:33 +0700

    [Feat] Bind r button to reset

--- Pong.srcs/sources_1/new/inputLogic.v
+++ Pong.srcs/sources_1/new/inputLogic.v
@@ -23,7 +23,7 @@ module inputLogic(
     input clk,
     input RsRx,
     output RsTx,
-    output [4:0] ws85l
+    output [5:0] ws85lr
     );
     
     reg en, last_rec;
@@ -31,10 +31,13 @@ module inputLogic(
     reg [3:0] movement = 4'b0000; // 1st player up/down, 2nd player up/down
     reg l = 1'b0; // l key
     reg lState = 1'b0; // l key state
+    reg r = 1'b0; // r key
+    reg rState = 1'b0; // r key state
+    
     wire [7:0] data_out;
     wire sent, received, baud;
     
-    assign ws85l = {movement,l}; // assign movement and l to ws85l
+    assign ws85lr = {movement,l,r}; // assign movement, l, r to ws85lr
     
     baudrate_gen baudrate_gen(clk, baud); // generate baud rate
     uart_rx receiver(baud, RsRx, received, data_out); // encode data to 8 bits
@@ -49,7 +52,8 @@ module inputLogic(
             || data_in == 8'h38 // 8
             || data_in == 8'h35 // 5
             || data_in == 8'h6C // l
-            ) en = 1; // recieve only w,s,8,5,l keys
+            || data_in == 8'h72 // r
+            ) en = 1; // recieve only w,s,8,5,l,r keys
         end
         last_rec = received;
     end
@@ -61,6 +65,7 @@ module inputLogic(
                 8'h73: movement[3:2] = 2'b01; // s 1st player down
                 8'h38: movement[1:0] = 2'b10; // 8 2nd player up
                 8'h35: movement[1:0] = 2'b01; // 5 2nd player down
+                8'h72: movement = 4'b0000;
             endcase
         end
     end
@@ -75,6 +80,15 @@ module inputLogic(
                 l = 1'b0;
             end
             else lState = 1'b0; // l key released
+            
+            if(data_in == 8'h72 && rState == 1'b0) begin // l key pressed
+                r = 1'b1;
+                rState = 1'b1;
+            end
+            else if(data_in == 8'h72 && rState == 1'b1) begin // allow only 1 l key press (single pulse)
+                r = 1'b0;
+            end
+            else rState = 1'b0; // l key released
         end
     end
     
--- Pong.srcs/sources_1/new/main.v
+++ Pong.srcs/sources_1/new/main.v
@@ -23,7 +23,6 @@
 module main(
     input clk,
     input RsRx,
-    input btnU,
     input btnD,
     output RsTx,
     output [6:0] seg,
@@ -37,11 +36,10 @@ module main(
     
     wire [7:0] player1Score;
     wire [7:0] player2Score;
-
     wire [9:0] x,y;
-    wire player1Up, player1Down, player2Up, player2Down, throwBall;
+    wire player1Up, player1Down, player2Up, player2Down, throwBall, reset;
     wire [3:0] ps11, ps12, ps21, ps22;
-    wire [4:0] ws85l;
+    wire [5:0] ws85lr;
     wire [2:0] rgb_out;
     wire scorePlayer1Count;
     wire scorePlayer2Count;
@@ -50,7 +48,7 @@ module main(
     assign ps12 = player1Score[3:0]; // player1 score 2nd digit
     assign ps21 = player2Score[7:4]; // player2 score 1st digit
     assign ps22 = player2Score[3:0]; // player2 score 2nd digit
-    assign {player1Up,player1Down,player2Up,player2Down,throwBall} = ws85l; // assign ws85l to player1Up, player1Down, player2Up, player2Down, throwBall
+    assign {player1Up,player1Down,player2Up,player2Down,throwBall,reset} = ws85lr; // assign ws85l to player1Up, player1Down, player2Up, player2Down, throwBall
     assign vgaBlue = {rgb_out[2],rgb_out[2],rgb_out[2],rgb_out[2]}; // assign rgb_out to vgaBlue, vgaGreen, vgaRed
     assign vgaGreen = {rgb_out[1],rgb_out[1],rgb_out[1],rgb_out[1]};
     assign vgaRed = {rgb_out[0],rgb_out[0],rgb_out[0],rgb_out[0]};
@@ -59,12 +57,12 @@ module main(
         clk,
         RsRx,
         RsTx,
-        ws85l
+        ws85lr
     ); // keyboard input
 
     animationLogic animeLogic( // main game logic and animation logic
         clk,
-        btnU,// reset
+        reset,// reset
         x,// position x
         y,// position y
         1, //videoOn show video where the value is 1
@@ -80,8 +78,9 @@ module main(
         scorePlayer2Count // Adding score of player2
     );
 
-    vga vga_render(.clk(clk),
-        .reset(reset),
+    vga vga_render(
+        .clk(clk),
+        .reset(0),
         .hsync(Hsync),
         .vsync(Vsync),
         .video_on(video_on),
@@ -92,7 +91,7 @@ module main(
     
     scoreLogic scoreLogicModule (
         .clk(clk),
-        .reset(btnU), // reset
+        .reset(reset), // reset
         .scorePlayer1Count(scorePlayer1Count), // Adding score of player1
         .scorePlayer2Count(scorePlayer2Count), // Adding score of player2
         .player1Score(player1Score), // player1 score output
